b09c73a0ad362fc70bdc3fdfc01ec68bf379bf27
jalil <jalil@1001.digital>
2025-03-25 22:10:11 +0100

    Add Table

---
+++ base/components/Table.vue
@@ -0,0 +1,45 @@
+<template>
+  <table>
+    <slot />
+  </table>
+</template>
+
+<style scoped>
+table {
+  @mixin ui-font;
+  color: var(--muted);
+  height: min-content;
+
+  :slotted(thead) {
+    color: var(--color);
+  }
+
+  :slotted(tr:not(:last-child:not(:first-child))) {
+    border-bottom: var(--border);
+  }
+
+  :deep(td) {
+    white-space: nowrap;
+    padding: var(--size-1) var(--spacer-sm);
+
+    &:first-child {
+      padding-left: 0;
+    }
+    &:last-child {
+      padding-right: 0;
+    }
+
+    &.right {
+      text-align: right;
+    }
+  }
+
+  :slotted(tfoot) {
+    tr {
+      border-bottom: 0 !important;
+      border-top: var(--border);
+    }
+  }
+}
+</style>
+
