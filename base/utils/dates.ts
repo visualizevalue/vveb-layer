3b97a11d693b4dbe13e2a7c250e61a7531be4cd1
jalil <jalil@1001.digital>
2025-03-25 22:10:28 +0100

    Implement date helpers

---
+++ base/utils/dates.ts
@@ -0,0 +1,28 @@
+import { DateTime } from 'luxon'
+
+export const formatDate = (date: string) => {
+  return DateTime.fromISO(date)
+    .setLocale('en')
+    .toLocaleString(DateTime.DATE_MED)
+}
+
+export const formatDateTime = (date: string) => {
+  return DateTime.fromISO(date)
+    .setLocale('en-US')
+    .toLocaleString({ year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' })
+}
+
+export const formatTime = (date: string) => {
+  return DateTime.fromISO(date)
+    .setLocale('en-US')
+    .toLocaleString({ hour: 'numeric', minute: 'numeric' })
+}
+
+export const timeAgo = (time: string) => {
+  const date = DateTime.fromISO(time)
+
+  return DateTime.now().diff(date).as('days') > 2
+    ? formatDate(time)
+    : date.toRelative({ style: 'short', locale: 'us' })
+}
+
