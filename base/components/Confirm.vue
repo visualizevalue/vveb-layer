9eaea4fe90644a54d54a6da31a245b54582cacea
jalil <jalil@1001.digital>
2025-03-27 13:56:31 +0100

    Add confirmation component

---
+++ base/components/Confirm.vue
@@ -0,0 +1,42 @@
+<template>
+  <slot>
+    <Button @click="open = true">Trigger</Button>
+  </slot>
+
+  <Dialog
+    v-model:open="open"
+    class="confirm-modal"
+  >
+    <h1>{{ title }}</h1>
+    <p v-if="description">{{ description }}</p>
+
+    <Actions>
+      <Button @click="open = false">{{ cancel }}</Button>
+      <Button @click="() => emit('confirm')">{{ confirm }}</Button>
+    </Actions>
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+const open = defineModel<boolean>('open', { default: false })
+
+const emit = defineEmits(['confirm'])
+
+const {
+  title,
+  description,
+  cancel = 'Cancel',
+  confirm = 'Confirm',
+} = defineProps<{
+  title: string,
+  description?: string,
+  cancel?: string,
+  confirm?: string
+}>()
+</script>
+
+<style>
+.confirm-modal {
+  padding: var(--spacer-lg);
+}
+</style>
