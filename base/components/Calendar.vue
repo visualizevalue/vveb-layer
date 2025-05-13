52ca02ca4f16b473975337ca5dd0e3a5342c8fc2
jalil <jalil@1001.digital>
2025-05-13 16:48:40 +0200

    Implement Calendar component

---
+++ base/components/Calendar.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { CalendarDate, CalendarDateTime, ZonedDateTime } from '@internationalized/date'
+import {
+  CalendarCell,
+  CalendarCellTrigger,
+  CalendarGrid,
+  CalendarGridBody,
+  CalendarGridHead,
+  CalendarGridRow,
+  CalendarHeadCell,
+  CalendarHeader,
+  CalendarHeading,
+  CalendarNext,
+  CalendarPrev,
+  CalendarRoot,
+} from 'reka-ui'
+
+const date = defineModel<CalendarDate | CalendarDateTime | ZonedDateTime>()
+</script>
+
+<template>
+  <CalendarRoot v-slot="{ weekDays, grid }" v-model="date" fixed-weeks class="calendar">
+    <CalendarHeader class="header">
+      <CalendarPrev as-child>
+        <Button class="small unstyled">
+          <Icon type="chevron-left" />
+        </Button>
+      </CalendarPrev>
+      <CalendarHeading />
+      <CalendarNext as-child>
+        <Button class="small unstyled">
+          <Icon type="chevron-right" />
+        </Button>
+      </CalendarNext>
+    </CalendarHeader>
+    <CalendarGrid v-for="month in grid" :key="month.value.toString()">
+      <CalendarGridHead>
+        <CalendarGridRow>
+          <CalendarHeadCell v-for="day in weekDays" :key="day">{{ day }}</CalendarHeadCell>
+        </CalendarGridRow>
+      </CalendarGridHead>
+      <CalendarGridBody>
+        <CalendarGridRow v-for="(weekDates, index) in month.rows" :key="`weekDate-${index}`">
+          <CalendarCell
+            v-for="weekDate in weekDates"
+            :key="weekDate.toString()"
+            :date="weekDate"
+          >
+            <CalendarCellTrigger :day="weekDate" :month="month.value" />
+          </CalendarCell>
+        </CalendarGridRow>
+      </CalendarGridBody>
+    </CalendarGrid>
+  </CalendarRoot>
+</template>
