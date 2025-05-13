3524dc3bfaac45ac63be7971ba7cbb4afa79df21
jalil <jalil@1001.digital>
2025-05-13 21:20:16 +0200

    Date validity example

--- theme/.playground/pages/index.vue
+++ theme/.playground/pages/index.vue
@@ -2,14 +2,22 @@
   <article>
     <h1>Hello VV Theme</h1>
 
-    <Calendar v-model="date" />
+    <Calendar v-model="date" :is-date-unavailable="isDateUnavailable" />
 
     <span>{{ date }}</span>
   </article>
 </template>
 
 <script setup lang="ts">
+import type { CalendarRootProps } from 'reka-ui'
+import { today } from '@internationalized/date'
+
 const date = ref()
+
+const validUntil = today('UTC')
+const isDateUnavailable: CalendarRootProps['isDateUnavailable'] = (date) => {
+  return date.compare(validUntil) < 0
+}
 </script>
 
 <style scoped>
