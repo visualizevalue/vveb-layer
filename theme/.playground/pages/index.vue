826bf35ad15a0c1f8aa66c233a617f4bf057bea1
jalil <jalil@1001.digital>
2025-05-13 16:48:56 +0200

    Test calendar

--- theme/.playground/pages/index.vue
+++ theme/.playground/pages/index.vue
@@ -1,5 +1,20 @@
 <template>
   <article>
     <h1>Hello VV Theme</h1>
+
+    <Calendar v-model="date" />
+
+    <span>{{ date }}</span>
   </article>
 </template>
+
+<script setup lang="ts">
+const date = ref()
+</script>
+
+<style scoped>
+article {
+  display: grid;
+  gap: var(--spacer);
+}
+</style>
