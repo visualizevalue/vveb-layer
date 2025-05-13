c0d873e6ff2a215bc2459f6de6f912588a2bb94e
jalil <jalil@1001.digital>
2025-05-13 16:48:44 +0200

    Test component

--- base/.playground/app.vue
+++ base/.playground/app.vue
@@ -2,10 +2,14 @@
   <main>
     <h1>Hello World</h1>
     <p>
-      <ExpandableText text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?" />
+      <ExpandableText
+        text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      />
     </p>
 
     <Loading />
+
+    <Calendar v-model="date" />
 
     <Alert>
       <p>Test Alert</p>
@@ -30,7 +34,6 @@
       </Actions>
     </Modal>
 
-
     <Modal v-model:open="dialogOpen">
       <p>Hellooooo</p>
 
@@ -49,9 +52,14 @@
       <CardLink :to="`https://vv.xyz`" title="Foo" />
     </Card>
 
-    <Image src="https://ipfs.vv.xyz/ipfs/QmPCkh5Tq7jropNj9TuW2CWieHgD9F3kz2udtUN1i7THNa?filename=vv.png" />
+    <Image
+      alt="VV"
+      src="https://ipfs.vv.xyz/ipfs/QmPCkh5Tq7jropNj9TuW2CWieHgD9F3kz2udtUN1i7THNa?filename=vv.png"
+    />
 
-    <Embed src="https://ipfs.vv.xyz/ipfs/QmXX3zc7xfS5rCdpXySRy1YcQdhsnUEWFszq66CUDYyvSh?filename=bunney-square.mp4" />
+    <Embed
+      src="https://ipfs.vv.xyz/ipfs/QmXX3zc7xfS5rCdpXySRy1YcQdhsnUEWFszq66CUDYyvSh?filename=bunney-square.mp4"
+    />
   </main>
 </template>
 
@@ -59,6 +67,8 @@
 const dialogOpen = ref(false)
 const compatDialogOpen = ref(false)
 const popoverOpen = ref(false)
+
+const date = ref()
 </script>
 
 <style scoped>
